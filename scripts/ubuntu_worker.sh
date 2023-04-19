22e39283b32de64a479a11c0c726be6a5a2b5b10
hchoi3 <hyunmin.choi@axleinfo.com>
2023-04-19 14:21:49 -0400

    added kubectl installation to script

--- scripts/ubuntu_worker.sh
+++ scripts/ubuntu_worker.sh
@@ -55,3 +55,21 @@ sudo apt-get install terraform -y
 #disable ufw
 sudo ufw disable
 sudo systemctl stop ufw
+
+#kubectl 
+#updates packages and install packages needed to use the Kubernetes apt repository
+sudo apt-get update  
+sudo apt-get install -y ca-certificates curl
+
+#If you use Debian 9 (stretch) or earlier you would also need to install apt-transport-https:
+sudo apt-get install -y apt-transport-https
+
+#download google cloud public signing key
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+#Add the Kubernetes apt repository
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+#update apt packages with new reposiory and install kubectl
+sudo apt-get update
+sudo apt-get install -y kubectl
