89476c0f14bbd3c3d3b8a3cb64620f6b830525ff
hchoi3 <hyunmin.choi@axleinfo.com>
2023-05-25 10:38:18 -0400

    added new jenkins repo and key for install

--- scripts/jenkins_setup.sh
+++ scripts/jenkins_setup.sh
@@ -4,10 +4,13 @@
 sudo apt install openjdk-11-jdk -y
 
 #add repository key to system 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 #append repo to sources.list
-sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 #update
 sudo apt update -y 
